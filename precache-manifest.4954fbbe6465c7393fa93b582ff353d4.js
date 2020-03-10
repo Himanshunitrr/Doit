ddef3834ec863efc4b6049416ae251e69517fc6d
himanshu_nitrr <himanshumauryacrz@gmail.com>
2020-03-10 23:23:22 +0530

    Updates

---
+++ precache-manifest.4954fbbe6465c7393fa93b582ff353d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aa215bb9ddbc53e830da23ad51bb01",
+    "url": "/Doit/index.html"
+  },
+  {
+    "revision": "429a8016890005349216",
+    "url": "/Doit/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f32632a0c1cf465e35b",
+    "url": "/Doit/static/js/2.37296315.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Doit/static/js/2.37296315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429a8016890005349216",
+    "url": "/Doit/static/js/main.b8e14749.chunk.js"
+  },
+  {
+    "revision": "bf664af5d8c8acabedd6",
+    "url": "/Doit/static/js/runtime-main.1c60911d.js"
+  }
+]);
